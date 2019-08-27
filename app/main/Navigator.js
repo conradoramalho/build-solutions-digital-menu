4b4c83e3c2f992b40eef7ab3e49890676d91da93
conradoramalho <conrado.ramalho94@gmail.com>
2019-08-26 23:03:53 -0300

    refactor: remove unused variables

--- app/main/Navigator.js
+++ app/main/Navigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 import * as Screens from '../screens';
 
