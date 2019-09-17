bb933c8dd72742db0b8ded36bb1df37491d2a9b2
conradoramalho <conrado.ramalho94@gmail.com>
2019-09-16 23:38:33 -0300

    feat: add menu component

--- app/main/Navigator.js
+++ app/main/Navigator.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 import { createStackNavigator } from 'react-navigation-stack';
 import * as Screens from '../screens';
@@ -15,26 +14,3 @@ const RootStack = createStackNavigator(
 );
 
 export default createAppContainer(RootStack);
-
-// import React from 'react';
-// import { View, Text } from 'react-native';
-// import { createAppContainer } from 'react-navigation';
-// import { createStackNavigator } from 'react-navigation-stack';
-
-// class HomeScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Home kka</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const AppNavigator = createStackNavigator({
-//   Home: {
-//     screen: HomeScreen,
-//   },
-// });
-
-// export default createAppContainer(AppNavigator);
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import Menu from '../modules/Home/Menu/Menu';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
+        <Menu />
       </View>
     );
   }
