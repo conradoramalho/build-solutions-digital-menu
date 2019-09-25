df0a7a762a5a31603d43c7c9957161c0cda3e3e3
conradoramalho <conrado.ramalho94@gmail.com>
2019-09-24 23:34:08 -0300

    feat: dish card container

--- app/components/DishCard/DishCard.js
+++ app/components/DishCard/DishCard.js
@@ -1,19 +1,85 @@
 import React from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import styled from 'styled-components/native';
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  background-color: blue;
+  padding: 20px;
+`;
+
+const ImageWrapper = styled.View`
+  flex: 1;
+`;
+
+const Image = styled.Image`
+  height: 250px;
+  width: 100%;
+`;
+
+const Container = styled.View`
+  flex: 3;
+  flex-direction: column;
+  height: 250px;
+  background-color: red;
+  padding: 20px;
+  position: relative;
+`;
+
+const Title = styled.View`
+  flex: 1;
+  flex-direction: row;
+`;
+
+const Description = styled.View`
+  flex: 2;
+`;
+
+const Buttons = styled.View`
+  flex: 1;
+  flex-direction: row;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+`;
+
+const Price = styled.Text`
+  margin: 0 30px 0 auto;
+  font-size: 20px;
+`;
+
+const SeeMore = styled.Button`
+  width: 300px;
+`;
+
+const Add = styled.Button`
+  width: 300px;
+`;
 
 const DishCard = () => {
   return (
-    <View>
-      <Image source={{ uri: `http://placehold.jp/${100}x${100}.png` }} />
-      <Text>Gran Prime Burguer</Text>
-      <Text>R$ 37,00</Text>
-      <Text>
-        Hamburguer de picanha envolvido por uma massa de pizza, cebola
-        caramelizada, picles e cheddar
-      </Text>
-      <Button>Ver mais</Button>
-      <Button>Adicionar</Button>
-    </View>
+    <Wrapper>
+      <ImageWrapper>
+        <Image source={{ uri: `http://placehold.jp/${250}x${250}.png` }} />
+      </ImageWrapper>
+      <Container>
+        <Title>
+          <Name>Gran Prime Burguer</Name>
+          <Price>R$ 37,00</Price>
+        </Title>
+        <Description>
+          <Text>
+            Hamburguer de picanha envolvido por uma massa de pizza, cebola
+            caramelizada, picles e cheddar
+          </Text>
+        </Description>
+        <Buttons>
+          <SeeMore title="Ver mais" />
+          <Add title="Adicionar" />
+        </Buttons>
+      </Container>
+    </Wrapper>
   );
 };
 
