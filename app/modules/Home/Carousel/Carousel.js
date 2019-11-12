20b8004b2f11fbbabb98d174752f307f35f93e51
conradoramalho <conrado.ramalho94@gmail.com>
2019-11-12 20:48:14 -0300

    feat: add legend

--- app/modules/Home/Carousel/Carousel.js
+++ app/modules/Home/Carousel/Carousel.js
@@ -14,18 +14,33 @@ const DATA = [
   { title: "ABCDEFG" }
 ];
 
+const Container = styled.View`
+  position: relative;
+`;
+
 const ImageWrapper = styled.Image`
   width: 100%;
   height: 100%;
 `;
 
+const Title = styled.Text`
+  width: 100%;
+  height: 200px;
+  background-color: rgba(0, 0, 0, 0.4470588235294118);
+  position: absolute;
+  color: #fff;
+  font-size: 30px;
+  bottom: 0;
+  z-index: 5;
+`;
+
 function HomeCarousel() {
   const renderItem = ({ item, index }) => {
     return (
-      <View>
-        <Text>Alo mae</Text>
+      <Container>
+        <Title>Cream Cheese Bacon Rib</Title>
         <ImageWrapper source={HamburguerHome} />
-      </View>
+      </Container>
     );
   };
 
