44e2a197eebbe0cd9352d7c69b926fe1697d88fa
conradoramalho <conrado.ramalho94@gmail.com>
2019-11-13 23:04:12 -0300

    chore: add react native svg

--- app/modules/Home/index.js
+++ app/modules/Home/index.js
@@ -23,15 +23,6 @@ function HomeScreen({navigation}) {
   return (
     <Wrapper>
       <MenuWrapper>
-        <svg width={24} height={24}>
-          <g fill="none" transform="translate(3.5 3)">
-            <path
-              stroke="#E12537"
-              d="M0 9.5h2.3v8H0zm2.5 1.2h6.688c-.288 1.867-1.431 2.978-3.43 3.335h4.448l5.198-3.85c1.13-.473 1.935.689 1.468 1.49 0 0-4.993 4.625-6.372 4.625h-8v-5.6zM0 7.4h17M15.788 7.422a7.503 7.503 0 00-14.582.023c.057-.236 14.53-.236 14.582-.023z"
-            />
-            <rect width={1} height={2} x={8} fill="#E12537" rx={0.5} />
-          </g>
-        </svg>
         <Menu navigate={navigation.navigate} />
       </MenuWrapper>
       <SlideWrapper>
