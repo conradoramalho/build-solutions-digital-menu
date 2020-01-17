05642e54462350ff1ee8b1ceb2785529af793d85
conradoramalho <conrado.ramalho94@gmail.com>
2020-01-16 22:21:23 -0300

    feat: add https to api uri

---
+++ app/reducers/cartReducer copy.js
@@ -0,0 +1,24 @@
+import * as actions from '../actions/cartActions';
+
+const INITIAL_STATE = {
+  isOpen: false,
+  items: [],
+};
+
+function cartReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case actions.OPEN_CART:
+      return {...state, isOpen: true};
+    case actions.CLOSE_CART:
+      return {...state, isOpen: false};
+    case actions.ADD_CART_ITEM:
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    default:
+      return state;
+  }
+}
+
+export default cartReducer;
